5546cf439fc6864e1b12b2553933a42c3a828148
Ucodice-13 <rohit.sharma825@gmail.com>
2020-05-26 17:34:58 +0530

    my initial commit

---
+++ calculate.php
@@ -0,0 +1,40 @@
+<?php
+
+$deg = '20';
+$min = '31';
+$sec = '37';
+
+
+$degree = '0.7144444444';
+
+
+$test = DMStoDD($deg,$min,$sec);
+
+$conver_to_deg_min_sec = DDtoDMS($deg,$min,$sec);
+
+print_r($conver_to_deg_min_sec);
+
+
+
+
+function DMStoDD($deg,$min,$sec)
+{
+    // Converting DMS ( Degrees / minutes / seconds ) to decimal format
+    return $deg+((($min*60)+($sec))/3600);
+}    
+
+function DDtoDMS($dec)
+{
+    // Converts decimal format to DMS ( Degrees / minutes / seconds ) 
+    $vars = explode(".",$dec);
+    $deg = $vars[0];
+    $tempma = "0.".$vars[1];
+
+    $tempma = $tempma * 3600;
+    $min = floor($tempma / 60);
+    $sec = $tempma - ($min*60);
+
+    return array("deg"=>$deg,"min"=>$min,"sec"=>$sec);
+}    
+
+?>
---
+++ swap.php
@@ -0,0 +1,15 @@
+<?php  
+$a=234;  
+$b=345;  
+// using arithmetic operation  
+$a=$a*$b;  
+
+echo $a;
+$b=$a/$b;  
+
+echo "<br>".$b;
+
+$a=$a/$b;  
+echo "<br>Value of a: $a</br>";  
+echo "Value of b: $b</br>";  
+?>  
